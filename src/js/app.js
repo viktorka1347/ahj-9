6550da3b339e98143d7b7df1eab9ccab8a7440a2
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-02-02 12:07:26 +0300

    main
    Signed-off-by: FIRST_NAME LAST_NAME <MY_NAME@example.com>

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,8 @@
 import CollapseWidget from './task1/CollapseWidget';
 
-
 const collapseWidget = new CollapseWidget(
   document.getElementById('container1'),
   'Collapse',
   'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.',
 );
 collapseWidget.bindToDOM();
-
